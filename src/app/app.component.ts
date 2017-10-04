7789afcef408230668fe796114068cb4ccee8d3e
Kevin Weidner <weidnerk@gmail.com>
2017-10-03 21:27:47 -0400

    Add a different version of the ctor.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,19 +1,29 @@
 import { Component } from '@angular/core';
 import { Observable } from 'rxjs/Observable';
 import 'rxjs/add/observable/interval';
+import 'rxjs/add/operator/take';
+import 'rxjs/add/operator/map';
 
 @Component({
   selector: 'app-root',
   template: `
-  <h1>{{ clock | async }}</h1>
+  <h1>{{ clock$ }}</h1>
   `
 })
 export class AppComponent {
 
+  clock$;
   // we make clock of type Observable but the template won't be updated with
-  clock = Observable.interval(1000);
+  //clock = Observable.interval(1000);
 
+  // constructor() {
+  //   this.clock.subscribe(console.log.bind(console));
+  // }
   constructor() {
-    this.clock.subscribe(console.log.bind(console));
+    Observable.interval(1000)
+    .map((x) => x+1)
+    .subscribe((x) => {
+      this.clock$ = x;
+    });
   }
 }
